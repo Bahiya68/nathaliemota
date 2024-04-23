338c750120cd6db53dad7c18c84f30fc96c007ab
Bahiya68 <bahiya.atouioual@laposte.net>
2024-04-24 00:59:40 +0200

    Gallerie lightbox

--- wp-content/themes/sitephoto/single-photo.php
+++ wp-content/themes/sitephoto/single-photo.php
@@ -6,7 +6,7 @@
         <div class="info">
             <h2><?php the_title(); ?></h2>
             <p>RÉFÉRENCE : <?php the_field('reference'); ?></p>
-            <p>CATÉGORIE : <?php $categorie = get_the_term_list($post_id, 'categorie');
+            <p>CATÉGORIE : <?php $categorie = get_the_term_list($post->ID, 'categorie');
                             if ($categorie) {
                                 echo $categorie;
                             } else {
@@ -14,7 +14,7 @@
                             }
                             ?></p>
             <p>FORMAT : <?php
-                        $format = get_the_term_list($post_id, 'format');
+                        $format = get_the_term_list($post->ID, 'format');
                         if ($format) {
                             echo $format;
                         } else {
@@ -27,8 +27,7 @@
 
 
         <div class="photos-container">
-            <img class="Card-image " src="<?php echo get_the_post_thumbnail_url(); ?>"
-                alt="<?php echo get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', true); ?>">
+            <img class="Card-image " src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php echo get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', true); ?>">
         </div>
     </div>
 
@@ -45,17 +44,14 @@
         <div class="part2">
             <div class="photominiature">
                 <div class="miniimg">
-                    <img src="<?php echo get_stylesheet_directory_uri() . './assets/images/nathalie-0.jpeg.webp'; ?>"
-                        alt="Santé !">
+                    <img src="<?php echo get_stylesheet_directory_uri() . './assets/images/nathalie-0.jpeg.webp'; ?>" alt="Santé !">
                 </div>
                 <div class="fleche_nav">
                     <div class="flechegauche_nav">
-                        <img src="<?php echo get_stylesheet_directory_uri() . './assets/images/flechegauche.svg'; ?>"
-                            alt="flechegauche">
+                        <img src="<?php echo get_stylesheet_directory_uri() . './assets/images/flechegauche.svg'; ?>" alt="flechegauche">
                     </div>
                     <div class="flechedroite_nav">
-                        <img src="<?php echo get_stylesheet_directory_uri() . './assets/images/flechedroite.svg'; ?>"
-                            alt="flechedroite">
+                        <img src="<?php echo get_stylesheet_directory_uri() . './assets/images/flechedroite.svg'; ?>" alt="flechedroite">
                     </div>
                 </div>
             </div>
@@ -68,77 +64,69 @@
         </div>
         <div class="likephoto">
             <?php
-        $args = array(
-            'post_type' => 'photo', // Type de contenu (article)
-            'posts_per_page' => 2, // Nombre d'articles à afficher
-        );
+            $args = array(
+                'post_type' => 'photo', // Type de contenu (article)
+                'posts_per_page' => 2, // Nombre d'articles à afficher
+            );
 
-        $query = new WP_Query($args);
+            $query = new WP_Query($args);
 
-        if ($query->have_posts()) :
-            while ($query->have_posts()) :
-                $query->the_post();
+            if ($query->have_posts()) :
+                while ($query->have_posts()) :
+                    $query->the_post();
 
-        ?>
+            ?>
 
 
-            <div class="photo-block">
-                <?php if (has_post_thumbnail()) : ?>
-                <div class="photopost">
-                    <a href="<?php echo get_the_permalink(get_the_ID()); ?>"
-                        data-post-title="<?php echo esc_attr(get_the_title(get_the_ID())); ?>">
-                        <?php the_post_thumbnail('medium', array('class' => 'post-thumbnail')); ?>
-                    </a>
-                </div>
-                <?php endif; ?>
+                    <div class="photo-block">
+                        <?php if (has_post_thumbnail()) : ?>
+                            <div class="photopost">
+                                <a href="<?php echo get_the_permalink(get_the_ID()); ?>" data-post-title="<?php echo esc_attr(get_the_title(get_the_ID())); ?>">
+                                    <?php the_post_thumbnail('medium', array('class' => 'post-thumbnail')); ?>
+                                </a>
+                            </div>
+                        <?php endif; ?>
 
-                <div class="photo-overlay">
-                    <h2 class="photo-title"><?php the_field('reference'); ?></h2>
-                    <h3 class="photo-categorie"><?php echo strip_tags(get_the_term_list($post->ID, 'categorie')); ?>
-                    </h3>
-                    <div class="eye-icon">
-                        <a class="eye-link" href="<?php the_permalink() ?>" class="eye-link">
-                            <img class="eye-image"
-                                src="<?php echo get_stylesheet_directory_uri() . '/assets/images/Icon_eye.png'; ?>"
-                                alt="Icone oeil pour voir la photo">
-                        </a>
+                        <div class="photo-overlay">
+                            <h2 class="photo-title"><?php the_field('reference'); ?></h2>
+                            <h3 class="photo-categorie"><?php echo strip_tags(get_the_term_list($post->ID, 'categorie')); ?>
+                            </h3>
+                            <div class="eye-icon">
+                                <a class="eye-link" href="<?php the_permalink() ?>" class="eye-link">
+                                    <img class="eye-image" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/Icon_eye.png'; ?>" alt="Icone oeil pour voir la photo">
+                                </a>
+                            </div>
+                            <div class="fullscreen-icon lightbox-enabled">
+                                <img class="fullscreen-image" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/Icon_fullscreen.png'; ?>" alt="Icone fullscreen pour voir la photo en grand">
+                            </div>
+                        </div>
+                        <div class="lightbox-container">
+
+                            <span class="material-symbols-outlined material-icons lightbox-btn left" id="left">
+                                <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/arrows_gauche.png'; ?>" alt="précedent">
+                            </span>
+                            <span class="material-symbols-outlined material-icons lightbox-btn right" id="right">
+                                <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/arrows_droite.png'; ?>" alt="suivant">
+                            </span>
+                            <span id="close" class="close material-icons material-symbols-outlined">
+                                <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/Cross.png'; ?>" alt="fermer">
+                            </span>
+                            <div class="lightbox-image-wrapper">
+                                <img alt="lightboximage" class="lightbox-image">
+                            </div>
+
+                        </div>
                     </div>
-                    <div class="fullscreen-icon lightbox-enabled">
-                        <img class="fullscreen-image"
-                            src="<?php echo get_stylesheet_directory_uri() . '/assets/images/Icon_fullscreen.png'; ?>"
-                            alt="Icone fullscreen pour voir la photo en grand">
-                    </div>
-                </div>
-                <div class="lightbox-container">
-
-                    <span class="material-symbols-outlined material-icons lightbox-btn left" id="left">
-                        <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/arrows_gauche.png'; ?>"
-                            alt="précedent">
-                    </span>
-                    <span class="material-symbols-outlined material-icons lightbox-btn right" id="right">
-                        <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/arrows_droite.png'; ?>"
-                            alt="suivant">
-                    </span>
-                    <span id="close" class="close material-icons material-symbols-outlined">
-                        <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/Cross.png'; ?>"
-                            alt="fermer">
-                    </span>
-                    <div class="lightbox-image-wrapper">
-                        <img alt="lightboximage" class="lightbox-image">
-                    </div>
-
-                </div>
-            </div>
 
 
             <?php
-            endwhile;
-            wp_reset_postdata(); // Réinitialise la requête
-        else :
-            echo 'Aucun article trouvé.';
-        endif;
+                endwhile;
+                wp_reset_postdata(); // Réinitialise la requête
+            else :
+                echo 'Aucun article trouvé.';
+            endif;
 
-        ?>
+            ?>
         </div>
         <!-- <div class="card_photos">
             <div class="lightbox_img_wrap">
--- wp-content/themes/sitephoto/template-parts/gallerie_photos.php
+++ wp-content/themes/sitephoto/template-parts/gallerie_photos.php
@@ -15,13 +15,16 @@ if ($query->have_posts()) :
 
 <div class="photo-block">
     <?php if (has_post_thumbnail()) : ?>
-    <div class="photopost">
-        <a href="<?php echo get_the_permalink(get_the_ID()); ?>"
-            data-post-title="<?php echo esc_attr(get_the_title(get_the_ID())); ?>">
+
+    <div class=" photopost">
+        <a data-href="<?php echo get_the_permalink(get_the_ID()); ?>">
             <?php the_post_thumbnail('medium', array('class' => 'post-thumbnail')); ?>
+
         </a>
+
     </div>
-    <?php endif; ?>
+
+
 
     <div class="photo-overlay">
         <h2 class="photo-title"><?php the_field('reference'); ?></h2>
@@ -33,13 +36,16 @@ if ($query->have_posts()) :
                     alt="Icone oeil pour voir la photo">
             </a>
         </div>
-        <div class="fullscreen-icon lightbox-enabled">
+
+        <div class="fullscreen-icon lightbox-enabled"
+            data-imgsrc="<?php echo esc_url(get_the_post_thumbnail_url(get_the_ID())); ?>"
+            data-post-title="<?php echo esc_attr(get_the_title(get_the_ID())); ?>">
             <img class="fullscreen-image"
-                src="<?php echo get_stylesheet_directory_uri() . '/assets/images/Icon_fullscreen.png'; ?>"
-                alt="Icone fullscreen pour voir la photo en grand">
+                src=" <?php echo get_stylesheet_directory_uri() . '/assets/images/Icon_fullscreen.png'; ?>"
+                alt=" Icone fullscreen pour voir la photo en grand">
         </div>
     </div>
-    <div class="lightbox-container">
+    <section class="lightbox-container">
 
         <span class="material-symbols-outlined material-icons lightbox-btn left" id="left">
             <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/arrows_gauche.png'; ?>"
@@ -53,12 +59,14 @@ if ($query->have_posts()) :
         </span>
         <div class="lightbox-image-wrapper">
             <img alt="lightboximage" class="lightbox-image">
+
+
         </div>
 
-    </div>
+    </section>
 </div>
 
-
+<?php endif; ?>
 <?php
     endwhile;
     wp_reset_postdata(); // Réinitialise la requête
