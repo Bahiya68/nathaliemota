028ed1694bf100032d040c15f716e3e72310d85d
Bahiya68 <bahiya.atouioual@laposte.net>
2024-05-18 22:42:23 +0200

    reglage banner

--- wp-content/themes/sitephoto/template-parts/banner.php
+++ wp-content/themes/sitephoto/template-parts/banner.php
@@ -1,5 +1,29 @@
 <div class="banner">
     <h1>PHOTOGRAPHE EVENT</h1>
-    <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/nathalie-3.jpeg.webp'; ?>" alt="Tout est installé">
-    </a>
+    <?php
+    // arguments de la requête
+    $args = array(
+        'post_type' => 'photo',
+        'posts_per_page' => 1,
+        'orderby' => 'rand',
+    );
+
+    // création d' une nouvelle instance de WP_Query
+    $query = new WP_Query($args);
+
+    // boucle sur les résultats
+    if ($query->have_posts()) {
+        while ($query->have_posts()) {
+            $query->the_post();
+
+
+            echo '<img class="banner__background" src="';
+            echo the_post_thumbnail_url();
+            echo '" />';
+        }
+    }
+    // réinitialisation de la requête
+    wp_reset_postdata();
+    ?>
+
 </div>
